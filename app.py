4ac83ef27bbb80accd129e41493abb5800544012
Muhammad Rizwan <135311147+mrizwan0094@users.noreply.github.com>
2025-01-13 00:58:11 +0500

    allow_credentials

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://rizwan.power-devs.com"],  # Add your frontend URL here
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
